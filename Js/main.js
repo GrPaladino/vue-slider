34aa64b04d23ad934234d0c2628b4382d718b475
GrPaladino <paladino.graziano@gmail.com>
2024-01-30 14:36:02 +0100

    Feat: add next and previous image on clicks on arrows "next" and  "prev"

--- Js/main.js
+++ Js/main.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      slideCounter: 0,
+      activeImage: 0,
       slideClass: "active",
       slides: [
         {
@@ -36,109 +36,124 @@ createApp({
   },
 
   methods: {
-    // ## CHIEDI UN NUMERO
-    /**
-     *
-     * @param {string} msg Messaggio da stampare nel prompt
-     * @returns {number} Numero fornito dall'utente
-     */
-    askNumber(msg = "Inserisci un numero") {
-      let userNumber = parseInt(prompt(msg));
+    // // ## CHIEDI UN NUMERO
+    // /**
+    //  *
+    //  * @param {string} msg Messaggio da stampare nel prompt
+    //  * @returns {number} Numero fornito dall'utente
+    //  */
+    // askNumber(msg = "Inserisci un numero") {
+    //   let userNumber = parseInt(prompt(msg));
 
-      while (isNaN(userNumber)) {
-        userNumber = parseInt(prompt("Il numero inserito non é valido"));
-      }
+    //   while (isNaN(userNumber)) {
+    //     userNumber = parseInt(prompt("Il numero inserito non é valido"));
+    //   }
 
-      return userNumber;
-    },
+    //   return userNumber;
+    // },
 
-    // ## GENERA UN NUMERO RANDOMICO
-    /**
-     *
-     * @param {number} min Il numero minimo
-     * @param {number} max Il numero massimo
-     * @returns Numero randomico tra il minimo e il massimo
-     */
-    getRandomNumber(min, max) {
-      min = parseInt(min);
-      max = parseInt(max);
+    // // ## GENERA UN NUMERO RANDOMICO
+    // /**
+    //  *
+    //  * @param {number} min Il numero minimo
+    //  * @param {number} max Il numero massimo
+    //  * @returns Numero randomico tra il minimo e il massimo
+    //  */
+    // getRandomNumber(min, max) {
+    //   min = parseInt(min);
+    //   max = parseInt(max);
 
-      if (isNaN(min) || isNaN(max)) {
-        console.error("I valori inseriti devono essere numerici");
-        return;
-      }
+    //   if (isNaN(min) || isNaN(max)) {
+    //     console.error("I valori inseriti devono essere numerici");
+    //     return;
+    //   }
 
-      if (min >= max) {
-        console.error(
-          "Il valore massimo deve essere maggiore del valore minimo"
-        );
-        return;
-      }
-      const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-      return randomNumber;
-    },
+    //   if (min >= max) {
+    //     console.error(
+    //       "Il valore massimo deve essere maggiore del valore minimo"
+    //     );
+    //     return;
+    //   }
+    //   const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    //   return randomNumber;
+    // },
 
-    // ## ADDIZIONA
-    /**
-     *
-     * @param {numero} primoAddendo Il primo numero dell'addizione
-     * @param {numero} secondoAddendo Il secondo numero dell'addizione
-     * @returns
-     */
-    addiziona(primoAddendo, secondoAddendo) {
-      const somma = primoAddendo + secondoAddendo;
-      return somma;
-    },
+    // // ## ADDIZIONA
+    // /**
+    //  *
+    //  * @param {numero} primoAddendo Il primo numero dell'addizione
+    //  * @param {numero} secondoAddendo Il secondo numero dell'addizione
+    //  * @returns
+    //  */
+    // addiziona(primoAddendo, secondoAddendo) {
+    //   const somma = primoAddendo + secondoAddendo;
+    //   return somma;
+    // },
 
-    // ## MOLTIPLICAZIONE
-    /**
-     *
-     * @param {numero} primoFattore Il primo numero da moltiplicare
-     * @param {numero} secondoFattore Il secondo numero da moltiplicare
-     * @returns Il risultato della moltiplicazione
-     */
-    moltiplica(primoFattore, secondoFattore) {
-      let risultato = 0;
+    // // ## MOLTIPLICAZIONE
+    // /**
+    //  *
+    //  * @param {numero} primoFattore Il primo numero da moltiplicare
+    //  * @param {numero} secondoFattore Il secondo numero da moltiplicare
+    //  * @returns Il risultato della moltiplicazione
+    //  */
+    // moltiplica(primoFattore, secondoFattore) {
+    //   let risultato = 0;
 
-      for (let i = 0; i < secondoFattore; i++) {
-        risultato = addiziona(risultato, primoFattore);
-      }
-      return risultato;
-    },
+    //   for (let i = 0; i < secondoFattore; i++) {
+    //     risultato = addiziona(risultato, primoFattore);
+    //   }
+    //   return risultato;
+    // },
 
-    // ## GENERA UNA GRIGLIA
-    generateGrid(container) {
-      container.innerHTML = "";
+    // // ## GENERA UNA GRIGLIA
+    // generateGrid(container) {
+    //   container.innerHTML = "";
 
-      for (let i = 0; i < 64; i++) {
-        const cellElement = generateCell(i);
-        container.append(cellElement);
-      }
-    },
+    //   for (let i = 0; i < 64; i++) {
+    //     const cellElement = generateCell(i);
+    //     container.append(cellElement);
+    //   }
+    // },
 
-    // ## GENERA CELLA NUMERATA
-    generateCell(index) {
-      const cell = document.createElement("div");
-      cell.classList.add("box");
-      cell.setAttribute("data-index", index);
+    // // ## GENERA CELLA NUMERATA
+    // generateCell(index) {
+    //   const cell = document.createElement("div");
+    //   cell.classList.add("box");
+    //   cell.setAttribute("data-index", index);
 
-      cell.addEventListener("click", function () {
-        const number = parseInt(this.getAttribute("data-index"));
+    //   cell.addEventListener("click", function () {
+    //     const number = parseInt(this.getAttribute("data-index"));
 
-        if (number % 2 === 0) {
-          this.classList.toggle("even");
-        } else {
-          this.classList.toggle("odd");
-        }
-      });
+    //     if (number % 2 === 0) {
+    //       this.classList.toggle("even");
+    //     } else {
+    //       this.classList.toggle("odd");
+    //     }
+    //   });
 
-      if (this.classList.contains("even") || this.classList.contains("odd")) {
-        this.innerTex = number;
+    //   if (this.classList.contains("even") || this.classList.contains("odd")) {
+    //     this.innerTex = number;
+    //   } else {
+    //     this.innerTex = "";
+    //   }
+
+    //   return cell;
+    // },
+
+    nextClick() {
+      if (this.activeImage >= this.slides.length - 1) {
+        this.activeImage = 0;
       } else {
-        this.innerTex = "";
+        this.activeImage++;
       }
-
-      return cell;
+    },
+    prevClick() {
+      if (this.activeImage <= 0) {
+        this.activeImage = this.slides.length - 1;
+      } else {
+        this.activeImage--;
+      }
     },
   },
 }).mount("#app");
